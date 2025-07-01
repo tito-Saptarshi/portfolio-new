f576af64db233aad35239fa5c18d5cafb9e4c63c
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2025-07-02 03:47:08 +0530

    general -u 02072025

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { projects, skills, socialLinks } from '../lib/constants'
-import { SidebarTrigger } from '@/components/ui/sidebar'
 
 export const Hero = () => {
   return (
